d2ea9b613ba6696476f0567d40ad6ac76f419cfe
Kris <kristianzwartdesign@gmail.com>
2021-04-15 14:44:40 +0200

    update

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -7,7 +8,7 @@ const courses = [
 { id: 1, name: 'course1' }
 { id: 2, name: 'course2' }
 { id: 3, name: 'course3' }
-];
+}];
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -18,6 +19,23 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) {
+    res.status(400).send(result.error);
+    return;
+    }
+
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minimum 3 characters')
+    return;
+    }
+
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
